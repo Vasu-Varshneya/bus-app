138071e149bae3cecaa22b76d084489bcb7cefe3
Shubham Garg <142537083+Immortal-CyberGuy@users.noreply.github.com>
2024-08-25 19:14:41 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+// components/Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">Nearby</a></li>
+        <li><a href="#">Around Me</a></li>
+        <li><a href="#">Wallet</a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
