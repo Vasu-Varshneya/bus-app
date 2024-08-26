f73fac7ca0e8d1d0a3e32767b1c0756eee7e1005
Vasu-Varshneya <148642918+Vasu-Varshneya@users.noreply.github.com>
2024-08-26 11:18:45 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
-// components/Footer.js
 import React from 'react';
-import './Footer.css';
-
+import '../pages/Home.css';
 const Footer = () => {
-  return (
-    <footer>
-      <ul>
-        <li><a href="#">Nearby</a></li>
-        <li><a href="#">Around Me</a></li>
-        <li><a href="#">Wallet</a></li>
-      </ul>
-    </footer>
-  );
+    return (
+        <footer className='min-h-20 w-full space-y-4 py-2 mx-auto '>
+            <ul className='flex justify-center items-center gap-12  '>
+                <li className=' font-bold text-zinc-400'><a href="#">
+                    <span>
+                        <img width={25} height={15} src="/mosque_6684648.png" alt="vv" />
+                    </span>
+                    Nearby</a></li>
+                <li className='font-bold text-zinc-400'><a href="#">
+                    <span>
+                        <img width={25} height={15} src="/mosque_6684648.png" alt="vv" />
+                    </span>
+                    Around Me</a></li>
+                <li className='font-bold text-zinc-400'><a href="#">
+                    <span>
+                        <img width={25} height={15} src="/wallet_3258446.png" alt="vv" />
+                    </span>
+                    Wallet</a></li>
+            </ul>
+        </footer>
+    );
 };
 
 export default Footer;
